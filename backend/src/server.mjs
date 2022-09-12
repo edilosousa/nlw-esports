4afe1fe93435be12e7678da3412a43024186aa8b
edilosousa <edilo13@gmail.com>
2022-09-12 17:15:12 -0400

    inicio do backend

---
+++ backend/src/server.mjs
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/ads', (req, res) => {
+    return res.json([
+        {
+            id: '1', name: 'User 1'
+        },
+        {
+            id: '2', name: 'User 2'
+        },
+        {
+            id: '3', name: 'User 3'
+        }
+    ])
+})
+
+app.listen(3333)
